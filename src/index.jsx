5c1f47078875a4486a852817f40e7f0cd96224f7
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-06 14:17:29 -0600

    run with succes react

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+// import react
+import React from 'react'
+// import render method
+import { render } from 'react-dom';
+
+// create root const
+const root = document.getElementById('root');
+
+// render the app
+render(<h1>I'm the app and I'm alive</h1>, root);
