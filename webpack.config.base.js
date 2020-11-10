aecd9b6333a27f9300a8fce8b9d54dcd10a705a9
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-10 13:17:10 -0600

    set up styles rules

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -6,10 +6,22 @@ const babelRules = {
   exclude: /node_modules/,
 };
 
+const styleRules = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    'style-loader',
+    // Translates CSS into CommonJS
+    'css-loader',
+    // Compiles Sass to CSS
+    'sass-loader',
+  ],
+};
+
 module.exports = {
   entry: './src/index.jsx',
   module: {
-    rules: [babelRules],
+    rules: [babelRules, styleRules],
   },
   plugins: [
     new HtmlWebpackPlugin({
