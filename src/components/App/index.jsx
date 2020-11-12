7889b29da19b5bf0ed589d5a35552ad0c79a5506
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-12 12:57:02 -0600

    add data to app component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,8 @@
 // adding components
 import { Player } from '../Player/index.jsx';
 import { Song } from '../Song/index.jsx';
+// import util
+import data from '../../utils/data';
 
 // create app component and export it
 export const App = () => (
